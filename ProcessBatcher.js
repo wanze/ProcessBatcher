06480dc606faa8bb000d2ab59c58ba2c3a3c3509
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2013-02-15 20:16:36 +0100

    Initial commit

---
+++ ProcessBatcher.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	
+	$('#batcherAction').change(function(){
+		var option = $(this).find('option:selected').val();
+		$('.batcherAction').closest('li.Inputfield').hide();
+		$('#wrap_'+option).show().css('list-style-type', 'none');
+	});
+	
+	$('input.toggle_all').click(function(){
+		if ($(this).prop('checked')) {
+			$('.toggle').prop('checked', true);
+		} else {
+			$('.toggle').prop('checked', false);
+		}
+	});
+	
+});
